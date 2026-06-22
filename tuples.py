52baf3874782a678b8612f7a8bc6aacc1be58bd8
Shree <kumarshreekant764@gmail.com>
2026-06-22 13:16:22 +0530

    day 3

---
+++ tuples.py
@@ -0,0 +1,12 @@
+tup = (4, 5 , 6, 7, 8)
+print(type(tup))
+print(tup[0])
+
+#tuples slicing
+print(tup[1:4])
+print(tup[:3])
+print(tup[3:])
+
+#tuples methods
+print(tup.count(7))
+print(tup.index(7))
